50851b7d10fb53bed7fd4d1843b320c7c8273d6d
Jay Wilhelm <jwilhelm@ohio.edu>
2021-03-03 12:48:10 -0500

    cleaned up prints

--- IMUStructure/IMUHelper.hpp
+++ IMUStructure/IMUHelper.hpp
@@ -1,14 +1,14 @@
 #define radtodeg 57.29746936176985516473022441508
 //Macros to make dataoutput easier
 #define printtab(x) Serial.print(String(x) + "\t ");
-#define printline() Serial.println()
+#define printline(x) Serial.println(x)
 //Class to hold the acc/gyro tare values
 class IMU_Calibration
 {
   public:
     double gyro_x_tare, gyro_y_tare, gyro_z_tare;
     double acc_x_tare, acc_y_tare, acc_z_tare;
-    static const int cal_samples = 250;
+    static const int cal_samples = CAL_SAMPLES;
     IMU_Calibration()
     {
       acc_x_tare=acc_y_tare=acc_z_tare=0.0;
--- IMUStructure/IMUStructure.ino
+++ IMUStructure/IMUStructure.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#define CAL_SAMPLES 5
 #include "IMUHelper.hpp"
 //Delare the hardware driver here
 Adafruit_MPU6050 mpu;
@@ -30,10 +31,10 @@ void setup(void)
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   
   // Calibration: Takes reading over a set period and averages the values
-  Serial.println("Calibrating gyro, place on level surface and do not move.");
+  printline("Calibrating gyro, place on level surface and do not move.");
 
   // Start your calibration sequence for gyro
-  for (int cal_int = 0; cal_int <  MPU_Calibration.cal_samples; cal_int ++)
+  for (int cal_int = 0; cal_int < CAL_SAMPLES; cal_int ++)
   {
     // Get new sensor events with the readings
     sensors_event_t a, g, temp;
@@ -46,12 +47,12 @@ void setup(void)
     //time between calibration readings
     delay(1);                                   
   }
-  Serial.println();
+  printline();
   //take out that Z is pointing directly down
   MPU_Calibration.acc_z_tare -= 9.81;
   //These are the found calibration values...
-  Serial.println("Calibration complete...");
-  Serial.println("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
+  printline("Calibration complete...");
+  printline("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
   printtab(MPU_Calibration.acc_x_tare);
   printtab(MPU_Calibration.acc_y_tare);
   printtab(MPU_Calibration.acc_z_tare);
@@ -63,26 +64,26 @@ void setup(void)
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
   //dump our results
-  Serial.println("Raw read...");
-  Serial.println("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
-  Serial.print(String(a.acceleration.x) + "\t ");
-  Serial.print(String(a.acceleration.y) + "\t ");
-  Serial.print(String(a.acceleration.z) + "\t ");
-  Serial.print(String(g.gyro.x) + "\t ");
-  Serial.print(String(g.gyro.y) + "\t ");
-  Serial.print(String(g.gyro.z) + "\t ");
-  Serial.println();
-  Serial.println("Calibrated read...");
-  Serial.println("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
+  printline("Raw read...");
+  printline("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
+  printtab(a.acceleration.x);
+  printtab(a.acceleration.y);
+  printtab(a.acceleration.z);
+  printtab(g.gyro.x);
+  printtab(g.gyro.y);
+  printtab(g.gyro.z);
+  printline();
+  printline("Calibrated read...");
+  printline("Ax\t Ay\t Az\t Gx\t Gy\t Gz");
 
-  Serial.print(String(MPU_Calibration.acc_x_tare-a.acceleration.x) + "\t ");
-  Serial.print(String(MPU_Calibration.acc_y_tare-a.acceleration.y) + "\t ");
-  Serial.print(String(MPU_Calibration.acc_z_tare-a.acceleration.z) + "\t ");
-  Serial.print(String(MPU_Calibration.gyro_x_tare-g.gyro.x) + "\t ");
-  Serial.print(String(MPU_Calibration.gyro_y_tare-g.gyro.y) + "\t ");
-  Serial.print(String(MPU_Calibration.gyro_z_tare-g.gyro.z) + "\t ");
-  Serial.println();
-  delay(1000);
+  printtab(MPU_Calibration.acc_x_tare-a.acceleration.x);
+  printtab(MPU_Calibration.acc_y_tare-a.acceleration.y);
+  printtab(MPU_Calibration.acc_z_tare-a.acceleration.z);
+  printtab(MPU_Calibration.gyro_x_tare-g.gyro.x);
+  printtab(MPU_Calibration.gyro_y_tare-g.gyro.y);
+  printtab(MPU_Calibration.gyro_z_tare-g.gyro.z);
+  printline();
+  delay(5000);
 }
 
 #define OUTPUT_RAW6DOF                  true
@@ -130,7 +131,7 @@ void loop()
     //save this because of integration
     static double raw_g_pitch_angle_deg = 0;
     //do the pitch calc
-    raw_g_pitch_angle_deg += g.gyro.y * dt;
+    raw_g_pitch_angle_deg += g.gyro.x * dt;
     printtab(raw_g_pitch_angle_deg);
     //ROLL????
     //
@@ -142,7 +143,7 @@ void loop()
     //save this because of integration
     static double pitch_angle_deg = 0;
     //do the pitch calc
-    pitch_angle_deg += (MPU_Calibration.gyro_y_tare - g.gyro.y) * dt;
+    pitch_angle_deg += (MPU_Calibration.gyro_x_tare - g.gyro.x) * dt;
     printtab(pitch_angle_deg);
     //ROLL????
     //
@@ -156,18 +157,18 @@ void loop()
     raw_acc_y = a.acceleration.y;
     raw_acc_z = a.acceleration.z;
 
-    double accelPitch_rad  = atan2(raw_acc_y, raw_acc_z);
+    double accelPitch_rad  = atan2(raw_acc_x, raw_acc_z);
     printtab(accelPitch_rad * radtodeg);
   } 
   //Find the acclerometer vector
   if(OUTPUT_ACC_VECTOR_CALIBRATED)
   {
-    double Craw_acc_y, Craw_acc_z;
-    Craw_acc_y = MPU_Calibration.acc_y_tare - a.acceleration.y;
+    double Craw_acc_x, Craw_acc_z;
+    Craw_acc_x = MPU_Calibration.acc_x_tare - a.acceleration.x;
     Craw_acc_z = MPU_Calibration.acc_z_tare - a.acceleration.z;
     
     //INVERT Z !!!, otherwise you get +/- 180 flips
-    double accelPitch_rad  = atan2(Craw_acc_y, -Craw_acc_z);
+    double accelPitch_rad  = atan2(Craw_acc_x, -Craw_acc_z);
     printtab(accelPitch_rad * radtodeg);
     //ROLL????
     #warning roll using tared vector not done
@@ -181,7 +182,7 @@ void loop()
     //Invert the Z value to avoid gimbal lock unless your sensor is upside down
     //Calc. the gyro calibrated integrals for x and y
     //Apply the complementary filter
-    //Profit?
+    //Profit? Tendies?
 
 
     double CFpitch_angle_rad = 0;
